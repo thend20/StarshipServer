c2fd9e4aee9b81c6033bf53056d7053283f6f592
root <root@starbound.pwnspeak.com>
2014-01-14 02:52:58 -0500

    more fixes

--- Commands/Planet.cs
+++ Commands/Planet.cs
@@ -1,6 +1,5 @@
 ﻿/* 
  * Starship Server
- * Created by Zidonuke (zidonuke@gmail.com) and Crashdoom (crashdoom@avilance.com)
  * 
  * This file is a part of Starship Server.
  * Starship Server is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
