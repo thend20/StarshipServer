35f4a8851fdeb2ece0155318df9ed4098b77393a
root <root@starbound.pwnspeak.com>
2014-01-14 02:45:09 -0500

    version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Resources;
 [assembly: AssemblyTitle("Starship Server")]
 [assembly: AssemblyDescription("Starbound Server Wrapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Avilance Ltd.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Starship Server")]
-[assembly: AssemblyCopyright("Copyright © 2013 Avilance Ltd.")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -44,6 +44,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.*")]
-[assembly: AssemblyFileVersion("1.2.1.*")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.*")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
