6389f67486e935a57a5f29beddda01d1ae011d07
sleepless1 <aaron.ash428@gmail.com>
2014-01-17 08:11:04 -0500

    Created prototype for claim command

--- Commands/Claim.cs
+++ Commands/Claim.cs
@@ -1,12 +1,48 @@
-/*
-	Claim planet for build/destroy protection
-	Allow player to build
-	Disabled for guest
-	One claim per user, new claim removes previous
-
-	Commands:
-	/claim - claim current planet
-	/claim off - remove claim
-	/claim allow [user] - Whitelist user, allow them to build
-	/claim deny [user] - remove user from whitelist 
+/* 
+ * Starship Server
+ * 
+ * This file is a part of Starship Server.
+ * Starship Server is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Starship Server is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Starship Server. If not, see http://www.gnu.org/licenses/.
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.goodstuff.Starship.Commands
+{
+    [ChatCommand]
+    public class Claim : CommandBase
+    {
+        /*
+            Claim planet for build/destroy protection
+            Allow player to build
+            Disabled for guest
+            One claim per user, new claim removes previous
+
+            Commands:
+            /claim - claim current planet
+            /claim off - remove claim
+            /claim allow [user] - Whitelist user, allow them to build
+            /claim deny [user] - remove user from whitelist 
+        */
+        public Claim(Client client)
+        {
+            this.name = "claim";
+            this.HelpText = "Claim current planet\n" +
+                "\toff - remove claim\n" +
+                "\tallow [user] - Whitelist user, allow them to build on this planet\n" +
+                "\tdeny [user] - remove user from whitelist";
+
+            this.client = client;
+            this.player = client.playerData;
+        }
+        public override bool doProcess(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
